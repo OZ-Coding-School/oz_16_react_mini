703b59c6831913832f87a7f25c895da79dfc091b
Seungyeon Oh <alstmd9902@naver.com>
2026-01-15 14:15:23 +0900

    feat: movieListData를 상태로 관리하고 MovieCard에 영화 정보를 props로 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import MovieCard from "./components/MovieCard";
+import movieListData from "./data/movieListData.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 더미데이터(movieListData) 상태관리
+  const [movies, setMovies] = useState(movieListData.results);
+  console.log(movies);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section className="max-w-7xl mx-auto">
+        <ul className="grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-4">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              posterPath={movie.poster_path}
+              title={movie.title}
+              rating={movie.vote_average}
+            />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
