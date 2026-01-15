a863f5f2dc857e21e14843926c4cfd52a7ccaad9
김우진 <kedw158@gmail.com>
2026-01-15 16:04:38 +0900

    feat: mission1 1단계 미션 구현

---
+++ src/components/MovieDetail.jsx
@@ -0,0 +1,16 @@
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+function MovieDetail({ movie }) {
+  if (!movie) return <p>영화를 선택해주세요.</p>;
+
+  return (
+    <div className="movie-detail">
+      <img src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={movie.title} />
+      <h1>{movie.title}</h1>
+      <p>⭐ {movie.vote_average}</p>
+      <p>{movie.overview}</p>
+    </div>
+  );
+}
+
+export default MovieDetail;
